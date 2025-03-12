b35bbfcc6208be69da6a78c2a93d7d8cfb2a45c4
tony-0312 <jjtk2003@gmail.com>
2025-03-12 23:38:43 +0530

    First Commit

---
+++ CurrencyConvertor/src/main/java/com/mycompany/currencyconvertor/CurrencyConvertor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.currencyconvertor;
+
+/**
+ *
+ * @author user
+ */
+public class CurrencyConvertor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
